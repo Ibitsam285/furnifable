16b64d2e95d89e857cf4124235275b4120555427
Ibitsam285 <30135822@students.southwales.ac.uk>
2025-04-14 15:54:58 +0500

    Version Updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id "dev.flutter.flutter-gradle-plugin"
-    id 'com.google.gms.google-services' // ✅ Keep this for Firebase
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
+
 
 dependencies {
     implementation platform('com.google.firebase:firebase-bom:33.12.0')
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,37 @@
-plugins {
-    id 'com.google.gms.google-services' version '4.4.2' apply false
-}
-
-allprojects {
+pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
     }
 }
-
-rootProject.buildDir = "../build"
-
-subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
-    project.evaluationDependsOn(":app")
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
-tasks.register("clean", Delete) {
-    delete rootProject.buildDir
+android {
+    namespace = "com.example.furnifable"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.yourapp"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
 }
